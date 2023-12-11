22e8c35ab07c22e6a3ecf12c2b2496f6d7c3be39
Michael (Michael) Alelegn <michael.alelegn@kcl.ac.uk>
2023-12-11 21:32:51 +0000

    exercise3

--- things/models.py
+++ things/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 #in case not registered for second test
 class Thing(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.CharField(max_length=120, blank=True)
+    quantity = models.IntegerField(default=0, blank=False, choices=[(i, i) for i in range(101)])
 
     def __str__(self):
         return self.name
